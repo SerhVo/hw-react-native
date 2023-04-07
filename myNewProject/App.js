7eae93b21d7cafa7f87d1affbc8d2625aed2523b
Serhii Vorotnoi <svorotnoy@gmail.com>
2023-04-07 13:43:07 +0300

    fix2

--- myNewProject/App.js
+++ myNewProject/App.js
@@ -1,90 +1,80 @@
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from 'react';
 import * as Font from "expo-font";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import RegistrationScreen from "./screens/RegistrationScreen";
 import LoginScreen from "./screens/LoginScreen";
 import { Home } from "./screens/Home";
-import { useDispatch, useSelector } from "react-redux";
-import { currentUser } from "./redux/auth/authoperations";
 import { CommentsScreen } from "./screens/CommentsScreen";
 import { MapScreen } from "./screens/MapScreen";
 import { StyleSheet } from "react-native";
+import { AppLoading } from 'expo';
 
-const customFonts = {
-    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
-    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+
+
+const customFonts = async () => {
+    await Font.loadAsync({
+        "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+        "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    });
 };
 
 const Stack = createStackNavigator();
 
-export default () => {
-    const [fontsLoaded, setFontsLoaded] = useState(false);
+export default function App() {
+    const [isReady, setIsReady] = useState(false);
 
     useEffect(() => {
-        async function loadFontsAsync() {
-            await Font.loadAsync(customFonts);
-            setFontsLoaded(true);
-        }
-
-        loadFontsAsync();
+        customFonts().then(() => setIsReady(true));
     }, []);
-    if (!fontsLoaded) {
-        return null;
+
+    if (!isReady) {
+        return <AppLoading />;
     }
-    
-};
 
-const App = () => {
-    const dispatch = useDispatch();
-    const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
 
-    useEffect(() => {
-        dispatch(currentUser());
-    }, [isLoggedIn]);
-
-    return (       
-            <NavigationContainer>
-                <Stack.Navigator initialRouteName="Home">
-                            <Stack.Screen
-                                name="Registration"
-                                component={RegistrationScreen}
-                                options={{ headerShown: false }}
-                            />
-                            <Stack.Screen
-                                name="Login"
-                                component={LoginScreen}
-                                options={{ headerShown: false }}
-                            />
-                            <Stack.Screen
-                                name="Home"
-                                component={Home}
-                                options={{ headerShown: false }}
-                            />
-                            <Stack.Screen
-                                name="Comments"
-                                component={CommentsScreen}
-                                options={{
-                                    headerStyle: styles.headerBox,
-                                    headerPressColor: "#FF6C00",
-                                    headerTitleStyle: styles.headerTitle,
-                                    headerTitleAlign: "center",
-                                    title: "Comments",
-                                }}
-                            />
-                            <Stack.Screen
-                                name="Map"
-                                component={MapScreen}
-                                options={{
-                                    headerStyle: styles.headerBox,
-                                    headerPressColor: "#FF6C00",
-                                    headerTitleStyle: styles.headerTitle,
-                                    headerTitleAlign: "center",
-                                    title: "Map",
-                                }}
-                            />                                          
-                </Stack.Navigator>
-            </NavigationContainer>       
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen
+                    name="Registration"
+                    component={RegistrationScreen}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name="Login"
+                    component={LoginScreen}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name="Home"
+                    component={Home}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name="Comments"
+                    component={CommentsScreen}
+                    options={{
+                        headerStyle: styles.headerBox,
+                        headerPressColor: "#FF6C00",
+                        headerTitleStyle: styles.headerTitle,
+                        headerTitleAlign: "center",
+                        title: "Comments",
+                    }}
+                />
+                <Stack.Screen
+                    name="Map"
+                    component={MapScreen}
+                    options={{
+                        headerStyle: styles.headerBox,
+                        headerPressColor: "#FF6C00",
+                        headerTitleStyle: styles.headerTitle,
+                        headerTitleAlign: "center",
+                        title: "Map",
+                    }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 };
 const styles = StyleSheet.create({
--- myNewProject/screens/PostsScreen.js
+++ myNewProject/screens/PostsScreen.js
@@ -12,7 +12,7 @@ import { EvilIcons } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
 
 
-export const PostsScreen = ({  navigation }) => {
+export const PostsScreen = ({ navigation }) => {
 
     return (
         <View style={styles.container}>
@@ -26,64 +26,59 @@ export const PostsScreen = ({  navigation }) => {
                     <Text>{user.email}</Text>
                 </View>
             </View>
-            {posts && (
-                <FlatList
-                    data={posts}
-                    renderItem={({ item }) => (
-                        <View
-                            style={{
-                                marginBottom: 32,
-                            }}
-                        >
-                            <Image
-                                style={{ width: 343, height: 240, borderRadius: 8 }}
-                            />
-                            <Text style={styles.textTitle}>{item.title}</Text>
 
-                            <View style={styles.userCard}>
-                                <Pressable
-                                    onPress={() =>
-                                        navigation.navigate("Comments")
-                                    }
-                                    style={styles.commentInfo}
-                                >
-                                    <EvilIcons
-                                        name="comment"
-                                        size={32}
-                                        style={{
-                                            color: commentsNum[item.postId] ? "#FF6C00" : "#BDBDBD",
-                                        }}
-                                    />
-                                    <Text
-                                        style={{
-                                            color: commentsNum[item.postId] ? "#212121" : "#BDBDBD",
-                                        }}
-                                    >
-                                        {commentsNum[item.postId] || 0}
-                                    </Text>
-                                </Pressable>
-                                <Feather
-                                    name="map-pin"
-                                    size={24}
-                                    color="#BDBDBD"
-                                    style={{ marginRight: 3 }}
+            <FlatList
+                data={posts}
+                renderItem={({ item }) => (
+                    <View
+                        style={{
+                            marginBottom: 32,
+                        }}
+                    >
+                        <Image
+                            style={{ width: 343, height: 240, borderRadius: 8 }}
+                        />
+                        <Text style={styles.textTitle}>{item.title}</Text>
+
+                        <View style={styles.userCard}>
+                            <Pressable
+                                onPress={() =>
+                                    navigation.navigate("Comments")
+                                }
+                                style={styles.commentInfo}
+                            >
+                                <EvilIcons
+                                    name="comment"
+                                    size={32}
+                                    style={{
+                                        color: commentsNum[item.postId] ? "#FF6C00" : "#BDBDBD",
+                                    }}
                                 />
                                 <Text
-                                    style={styles.textLocation}
-                                    onPress={() =>
-                                        navigation.navigate("Map", {
-                                            latitude: item.location.latitude,
-                                            longitude: item.location.longitude,
-                                        })
-                                    }
+                                    style={{
+                                        color: commentsNum[item.postId] ? "#212121" : "#BDBDBD",
+                                    }}
                                 >
-                                    {item.position}
+                                    {commentsNum[item.postId] || 0}
                                 </Text>
-                            </View>
+                            </Pressable>
+                            <Feather
+                                name="map-pin"
+                                size={24}
+                                color="#BDBDBD"
+                                style={{ marginRight: 3 }}
+                            />
+                            <Text
+                                style={styles.textLocation}
+                                onPress={() =>
+                                    navigation.navigate("Map")
+                                }
+                            >                                
+                            </Text>
                         </View>
-                    )}
-                />
-            )}
+                    </View>
+                )}
+            />
         </View>
     );
 };
